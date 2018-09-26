16ab1ad0f37479097a1b4857b45c02cf747c39d9
viestur8 <twotigers@vivaldi.net>
2018-09-26 01:50:37 -0400

    synatx errors and extra branches were eradicated

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'quizes#index'
+
   devise_scope :user do
     get "signup", to: "devise/registrations#new"
     get "login", to: "devise/sessions#new"
     end
 
   devise_for :users, :controllers => {:registrations => "registrations"}
+
     
 
 
