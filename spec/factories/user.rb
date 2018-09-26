f9040abc21bcd71ed1586404b15987d393e81760
viestur8 <twotigers@vivaldi.net>
2018-09-25 21:43:05 -0400

    add users factory

---
+++ spec/factories/user.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    sequence(:name) { |n| "test#{n}" }
+    sequence(:email) { |n| "test#{n}@test.com" }
+    password '123456'
+    password_confirmation '123456'
+  end
