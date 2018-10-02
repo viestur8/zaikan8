361f7c15adfbc5b641d04bdb8d7d4f150fed2217
viestur8 <twotigers@vivaldi.net>
2018-10-02 17:00:30 -0400

    resolved merge discrepancies - models/question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
-  attr_accessible :content, :survey_id, :answers_attributes
+  attr_accessor :content, :survey_id, :answers_attributes
   belongs_to :survey
   has_many :answers
   accepts_nested_attributes_for :answers, allow_destroy: true
