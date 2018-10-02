d01f6cc974a758b4a1068f3e6234399e9277d41d
viestur8 <twotigers@vivaldi.net>
2018-10-02 16:55:44 -0400

    resolved merge discrepancies - quest.rb

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,5 +1,5 @@
 class Quest < ApplicationRecord
-  attr_accessible :name, :questions_attributes
+  attr_accessor :name, :questions_attributes
   has_many :questions
-  accepts_nested_attributes_for :questions, allow_destroy: true 
+  accepts_nested_attributes_for :questions, allow_destroy: true
 end
